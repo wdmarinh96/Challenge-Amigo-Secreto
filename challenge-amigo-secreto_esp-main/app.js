fe754207e27929e9ae573c5acc1fa477b0bb4cf8
Wolfang Marin <126631897+wdmarinh96@users.noreply.github.com>
2025-03-11 04:06:32 -0500

    Update app.js

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,7 +1,2 @@
 
 
-// Agregar evento al botón de reinicio cuando se carga el DOM
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("reiniciar").addEventListener("click", reiniciarLista);
-});
-
